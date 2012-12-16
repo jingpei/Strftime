c6e76ab3a0aed69fe74fcf71ae4c42cd0f9f4e50
Jing Pei <jingpei@sas.upenn.edu>
2012-12-15 22:24:31 -0500

    First commit

---
+++ main.js
@@ -0,0 +1,22 @@
+$(document).on('ready', function () {
+// THIS IS THE TOP OF YOUR PROGRAM
+
+
+
+// this is your way to get the text inside the text box:
+// $('#input').val();
+
+// this is how to check if the submit button is clicked:
+// $('#submit').on('click', function () {
+//     /* insert your function to run when the submit button is clicked /*
+// });
+function nameOfFunction (optional, args) {
+	// insert body of function
+}
+
+
+
+
+
+// THIS IS THE BOTTOM OF YOUR PROGRAM
+});
