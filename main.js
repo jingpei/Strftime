6018450917babf139b01e57924b7de8d8eba1c6d
Jing Pei <jingpei@sas.upenn.edu>
2012-12-17 00:57:13 -0500

    More conditional console logs

--- main.js
+++ main.js
@@ -3,21 +3,24 @@ $(document).on('ready', function() {
 
 
 
-// this is your way to get the text inside the text box:
-// $('#input').val();
+/* this is your way to get the text inside the text box:
+$('#input').val();
+*/
 
-// this is how to check if the submit button is clicked:
-// $('#submit').on('click', function () {
-//     /* insert your function to run when the submit button is clicked /*
-// });
+/* 
+this is how to check if the submit button is clicked:
+$('#submit').on('click', function () {
+     /* insert your function to run when the submit button is clicked /*
+ });
+ */
 
 $('#button').click(function() {
 	var today = new Date();
-	var mm = today.getMonth()+1;
-	var dd = today.getDate();
-	var yyyy = today.getFullYear();
-	var hour = today.getHours();
-	var min = today.getMinutes();
+	
+	var event = $('#event').val();
+	if($('#event').val()==""){
+		event = "This event";
+	}
 	
 	var date = $('#date').val().toString();
 	var temp = new Array();
@@ -32,38 +35,62 @@ $('#button').click(function() {
 	function calculateTime(args){
 		if(today >= dateIn){
 			var diff = today.getTime()-dateIn.getTime();
-			diff = diff/(1000*60*60*24);
+			diff = (diff/(1000*60*60*24)).toFixed(0);
+			console.log(diff); 
+			//Prints value of diff
 			
-			if(diff == 0){
-				console.log("This event happened today");
+			
+			if(diff == 0 && today.getDay() == dateIn.getDay()){
+				var hours = today.getTime()-dateIn.getTime();
+				hours = (hours/(1000*60*60));
+				if(hours > 0 && hours < 1){
+					var min = today.getTime()-dateIn.getTime();
+					min = (min/(1000*60)).toFixed(0);
+					console.log(event + " happened " + min + " minutes ago.");
+				}
+				else if(hours >= 1 && hours < 2){
+					console.log(event + " happened an hour ago.");
+				}
+				else{
+				console.log(event + " happened today.");
+				}
 			}
-			else if(diff == 1){
-				console.log("This event happened yesterday");
+			
+			else if(diff == 1 && today.getDay() != dateIn.getDay()){
+				console.log("This event happened yesterday.");
 			}
-			else if(diff > 1 && diff < 7){
+			
+			else if(diff >= 2 && diff < 7){
 				var weekdays = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-				console.log("This event happened " + weekdays[dateIn.getDay()] + ".");
+				console.log(event + " happened " + weekdays[dateIn.getDay()] + ".");
 			}
+			
 			else if(diff >= 7 && diff < 30){
-				console.log("This event happened a few weeks ago.");
+				console.log(event + " happened a few weeks ago.");
 			}
+			
 			else if(diff >= 30 && diff < 60){
-				console.log("This event happened a month ago.");
+				console.log(event + " happened a month ago.");
 			}
+			
 			else if(diff >= 60 && diff < 365){
-				console.log("This event happened a few months ago.");
+				console.log(event + " happened a few months ago.");
 			}
-			else if(diff >= 365 && diff < 365*2){
-				console.log("This event happened last year.");
+			
+			else if(today.getYear() > dateIn.getYear()){
+				console.log(event + " happened last year.");
 			}
-			else if(diff >= 365*2){
-				console.log("This event happened a few years ago.");
+			
+			else if(today.getYear()-dateIn.getYear() >= 2){
+				console.log(event + " happened a few years ago.");
 			}
 		}
+		
 		else{
-			alert("Please enter a past date.");
+			alert("Please enter a past event.");
 			return false;
 		}
+		
 	};
 	
 	calculateTime(date);
