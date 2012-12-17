257a4d7b2697eb02ea95be81f397c413c5ec5c6c
Jing Pei <jingpei@sas.upenn.edu>
2012-12-17 00:01:01 -0500

    Strftime calculation working

--- main.js
+++ main.js
@@ -13,19 +13,62 @@ $(document).on('ready', function() {
 
 $('#button').click(function() {
 	var today = new Date();
-	var mm=(today.getMonth()+1).toString();
-	var dd=(today.getDate()).toString();
-	var yyyy=(today.getFullYear()).toString();
-	var hour = (today.getHours());
-	var min =  today.getMinutes();
-	var date = (($('#date').val()).replace(/\//g,"")).toString() + ($('#hour').val()).toString() + ($('#minutes').val()).toString();
-	today = mm+dd+yyyy+hour+min;
+	var mm = today.getMonth()+1;
+	var dd = today.getDate();
+	var yyyy = today.getFullYear();
+	var hour = today.getHours();
+	var min = today.getMinutes();
+	
+	var date = $('#date').val().toString();
+	var temp = new Array();
+	temp = date.split("/");
+	var mmIn = temp[0];
+	var ddIn = temp[1];
+	var yyyyIn = temp[2];
+	var hourIn = $('#hour').val();
+	var minIn = $('#minutes').val();
+	var dateIn = new Date(yyyyIn, mmIn-1, ddIn, hourIn, minIn, 0);
+	
 	function calculateTime(args){
-		console.log(date);
-		console.log(today);
+		if(today >= dateIn){
+			var diff = today.getTime()-dateIn.getTime();
+			diff = diff/(1000*60*60*24);
+			
+			if(diff == 0){
+				console.log("This event happened today");
+			}
+			else if(diff == 1){
+				console.log("This event happened yesterday");
+			}
+			else if(diff > 1 && diff < 7){
+				var weekdays = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+				console.log("This event happened " + weekdays[dateIn.getDay()] + ".");
+			}
+			else if(diff >= 7 && diff < 30){
+				console.log("This event happened a few weeks ago.");
+			}
+			else if(diff >= 30 && diff < 60){
+				console.log("This event happened a month ago.");
+			}
+			else if(diff >= 60 && diff < 365){
+				console.log("This event happened a few months ago.");
+			}
+			else if(diff >= 365 && diff < 365*2){
+				console.log("This event happened last year.");
+			}
+			else if(diff >= 365*2){
+				console.log("This event happened a few years ago.");
+			}
+		}
+		else{
+			alert("Please enter a past date.");
+			return false;
+		}
 	};
+	
 	calculateTime(date);
 });
+
 
 
 
